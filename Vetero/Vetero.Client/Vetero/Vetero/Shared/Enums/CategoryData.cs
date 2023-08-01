ddd4dff842eab57b2d20486a128c8488e62406ab
Bartosz <bbaginski93@gmail.com>
2023-08-01 21:17:13 +0200

    Dane do wykresu jako dropdown list

---
+++ Vetero/Vetero.Client/Vetero/Vetero/Shared/Enums/CategoryData.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vetero.Shared.Enums
+{
+    public enum CategoryData
+    {
+        [Display(Description = "Temperatura")]
+        Temperature,
+        [Display(Description = "Wilgotność")]
+        Humidity,     
+        [Display(Description = "Prędkość wiatru")]
+        WindKph,
+        [Display(Description = "Opad deszczu")]
+        TotalPrecip_mm
+    }
+}
---
+++ Vetero/Vetero.Client/Vetero/Vetero/Shared/Helpers/ReflectionHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Vetero.Shared.Helpers
+{
+    public static class ReflectionHelper
+    {
+        public static List<string> GetPropertiesNames(object pObject)
+        {
+            List<string> propertyList = new List<string>();
+            if (pObject != null)
+            {
+                foreach (var prop in pObject.GetType().GetProperties())
+                {
+                    propertyList.Add(prop.Name);
+                }
+            }
+            return propertyList;
+        }
+    }
+}
